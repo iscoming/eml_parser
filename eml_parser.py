3493ef2ea39be71d6946403f559a0fcf6c0694a2
iscoming <iscoming888@gmail.com>
2012-11-23 14:56:11 +0800

    fix encoding error
    fix "UnicodeEncodeError: ‘gbk’ codec can’t encode character ..."

--- eml_parser.py
+++ eml_parser.py
@@ -1,4 +1,4 @@
-#-*- encoding: gb2312 -*-
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
 
 import os
@@ -25,65 +25,106 @@ def decode_email(msgfile):
 	
 	#parse and decode subject
 	subject = msg.get("subject")
-	h_subject = email.Header.Header(subject)
-	dh_subject = email.Header.decode_header(h_subject)
-	subject = dh_subject[0][0]
-	subjectcode = dh_subject[0][1]
-	if(subjectcode != None):
-		subject = unicode(subject,subjectcode)
-		
-	print "subject:", subject
+	try:
+		h_subject = email.Header.Header(subject)
+		dh_subject = email.Header.decode_header(h_subject)
+		subject = dh_subject[0][0]
+		subjectcode = dh_subject[0][1]
+		if(subjectcode != None):
+			#subject = unicode(subject,subjectcode)
+			subject = subject.decode(subjectcode,'ignore')
+	except:
+		subject = subject.decode('ascii','ignore')
+	finally:
+		print "subject:", subject.encode('GBK','ignore')
+	
+	
 	
 	#parse and decode from
-	hmail_from = email.utils.parseaddr(msg.get("from"))
-	ptr = hmail_from[1] .find('@')
-	username = hmail_from[1][:ptr]
-	domain = hmail_from[1] [ptr:]
-	print "from: ",username,domain
+	from_username = ""
+	from_domain = ""
+	try:
+		hmail_from = email.utils.parseaddr(msg.get("from"))
+		ptr = hmail_from[1] .find('@')
+		from_username = hmail_from[1][:ptr]
+		from_domain = hmail_from[1] [ptr:]
+	except:
+		from_username = ""
+		from_domain = ""
+	finally:
+		print "from: ",from_username,from_domain
+	
+
 	
 	#parse and decode to
-	if(msg.get("to") !=None) :
-		hmail_tos = msg.get("to").split(',')
-		for hmail_to in hmail_tos:
-			hmail_to = email.utils.parseaddr(hmail_to)
-			ptr = hmail_to[1] .find('@')
-			username = hmail_to[1][:ptr]
-			domain = hmail_to[1] [ptr:]
-			print "to: ",username,domain
+	to_username = ""
+	to_domain = ""
+	try:
+		if(msg.get("to") !=None) :
+			hmail_tos = msg.get("to").split(',')
+			for hmail_to in hmail_tos:
+				hmail_to = email.utils.parseaddr(hmail_to)
+				ptr = hmail_to[1] .find('@')
+				to_username = hmail_to[1][:ptr]
+				to_domain = hmail_to[1] [ptr:]
+				
+	except:
+		to_username = ""
+		to_domain = ""
+	finally:
+		print "to: ",to_username,to_domain
+
 	
 	#parse and decode Cc
-	if(msg.get("Cc") !=None) :
-		hmail_ccs = msg.get("Cc").split(',')
-		for hmail_cc in hmail_ccs:
-			hmail_cc = email.utils.parseaddr(hmail_cc)
-			ptr = hmail_cc[1] .find('@')
-			username = hmail_cc[1][:ptr]
-			domain = hmail_cc[1] [ptr:]
-			print "cc: ",username,domain
+	cc_username = ""
+	cc_domain = ""
+	try:
+		if(msg.get("Cc") !=None) :
+			hmail_ccs = msg.get("Cc").split(',')
+			for hmail_cc in hmail_ccs:
+				hmail_cc = email.utils.parseaddr(hmail_cc)
+				ptr = hmail_cc[1] .find('@')
+				cc_username = hmail_cc[1][:ptr]
+				cc_domain = hmail_cc[1][ptr:]
+	except:
+		cc_username = ""
+		cc_domain = ""
+	finally:
+		print "cc: ",cc_username,cc_domain
+		
+		
 	
 	#parse and decode Date
-	hmail_date = email.utils.parsedate(msg.get("Date"))
-	print "Date: ",hmail_date
+	hmail_date = ""
+	try:
+		hmail_date = email.utils.parsedate(msg.get("Date"))
+	except:
+		hmail_date = ""
+	finally:
+		print "Date: ",hmail_date
 	
+
 	#parse and decode sender ip
 	IP = ""
-	hmail_recv = msg.get("X-Originating-IP")
-	if(hmail_recv != None):
-		hmail_recv = hmail_recv.strip("[]")
-		IP = hmail_recv
-	else :
-		hmail_receiveds =  msg.get_all("Received")
-		if(hmail_receiveds !=None):
-			for hmail_received in hmail_receiveds:
-				m = re.findall(r'from[^\n]*\[(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])',hmail_received)
-				if(len(m)>=1):
-					hmail_recv = re.findall(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])',m[0])
-					if(len(hmail_recv)>=1):
-						IP = hmail_recv[0]
-	
-	print "IP:",IP
+	try:
+		hmail_recv = msg.get("X-Originating-IP")
+		if(hmail_recv != None):
+			hmail_recv = hmail_recv.strip("[]")
+			IP = hmail_recv
+		else :
+			hmail_receiveds =  msg.get_all("Received")
+			if(hmail_receiveds !=None):
+				for hmail_received in hmail_receiveds:
+					m = re.findall(r'from[^\n]*\[(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])',hmail_received)
+					if(len(m)>=1):
+						hmail_recv = re.findall(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])',m[0])
+						if(len(hmail_recv)>=1):
+							IP = hmail_recv[0]
+	except:
+		IP = ""
+	finally:
+		print "IP:",IP
 				
-	
 	#header end
 	print "+"*60
 	
@@ -94,48 +135,66 @@ def decode_email(msgfile):
 			continue
 
 		#content plain
-		if (part.get_content_type() == "text/plain"):
-			
-			code = part.get_content_charset()
-			if( code == None):
-				print part.get_payload()
-			else:
-				print unicode(part.get_payload(decode=True),code)
+		content_plain = ""
+		try:
+			if (part.get_content_type() == "text/plain"):
+				
+				code = part.get_content_charset()
+				if( code == None):
+					content_plain = part.get_payload()
+				else:
+					content_plain = part.get_payload(decode=True).decode(code,'ignore')
+					#print unicode(part.get_payload(decode=True),code)
+		except:
+			content_plain = ""
+		finally:
+			print content_plain.encode('GBK','ignore')
 			print "+"*60
-			continue
+			#continue
 			
 		#content html
-		if (part.get_content_type() == "text/html"):
-			
-			code = part.get_content_charset()
-			if( code == None):
-				print part.get_payload()
-			else:
-				print unicode(part.get_payload(decode=True),code)
+		content_html = ""
+		try:
+			if (part.get_content_type() == "text/html"):
+				
+				code = part.get_content_charset()
+				print code
+				if( code == None):
+					content_html = part.get_payload()
+				else:
+					content_html = part.get_payload(decode=True).decode(code,'ignore')
+					#print unicode(part.get_payload(decode=True),code)
+		except:
+			content_html = ""
+		finally:	
+			print content_html.encode('GBK','ignore')
 			print "+"*60
-			continue
+			#continue
 		
 		#attachment
-		if (part.get('Content-Disposition') != None):
-			# this part is an attachment
-			name = part.get_filename()
-		#name = part.get_param("filename")
-			
-			counter += 1
-			h = email.Header.Header(name)
-			dh = email.Header.decode_header(h)
-			filename = dh[0][0]
-			filecode = dh[0][1]
-			if(filecode != None):
-				filename = unicode(filename,filecode)
-			if not filename:
-				filename = 'part-%03d' % (counter)
-			print 'attachmentL:', filename
-			fp = open(os.path.join('./', filename), 'wb')
-			fp.write(part.get_payload(decode=True))
-			fp.close()
+		try:
+			if (part.get('Content-Disposition') != None):
+				# this part is an attachment
+				name = part.get_filename()
+				
+				counter += 1
+				h = email.Header.Header(name)
+				dh = email.Header.decode_header(h)
+				filename = dh[0][0]
+				filecode = dh[0][1]
+				if(filecode != None):
+					filename = filename.decode(filecode,'ignore')
+				if not filename:
+					filename = 'part-%03d' % (counter)
+				print 'attachmentL:', filename
+				fp = open(os.path.join('./', filename), 'wb')
+				fp.write(part.get_payload(decode=True))
+				fp.close()
+		except:
+			print "attachment parse error"
+		finally:
 			print "+"*60
-			continue
+			#continue
 	
 	print "="*60
 
